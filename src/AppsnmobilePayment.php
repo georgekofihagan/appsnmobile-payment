61f368562e75d34e158f2940a5364d0b42f7cb13
georgekofihagan <georgekofihagan@users.noreply.github.com>
2021-10-20 11:08:02 +0000

    Fix styling

--- src/AppsnmobilePayment.php
+++ src/AppsnmobilePayment.php
@@ -55,15 +55,14 @@ class AppsnmobilePayment
         return true;
     }
 
-    private function makeRequest($params, $transaction_type){
-
+    private function makeRequest($params, $transaction_type)
+    {
         $params['callback_url'] = config("appsnmobile-payment.{$transaction_type}_callback_url");
 
         return Http::timeout(60)->withoutVerifying()
             ->acceptJson()
             ->withBasicAuth(config('appsnmobile-payment.auth.basic.user'), config('appsnmobile-payment.auth.basic.pass'))
-            ->post(config('appsnmobile-payment.baseurl')."?endpoint=sendRequest",$params);
-
+            ->post(config('appsnmobile-payment.baseurl')."?endpoint=sendRequest", $params);
     }
 
     public function processDebitCallback(array $data)
