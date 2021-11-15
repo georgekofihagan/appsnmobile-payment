ef720e29da88a582f87e6660e8591c92a9e495a4
georgekofihagan <georgekofihagan@users.noreply.github.com>
2021-11-15 06:24:13 +0000

    Fix styling

--- src/Models/MomoTransaction.php
+++ src/Models/MomoTransaction.php
@@ -63,6 +63,6 @@ class MomoTransaction extends Model
 
     protected static function newFactory()
     {
-        return new MomoTransactionFactory;
+        return new MomoTransactionFactory();
     }
 }
